a5f5c9826387de955b30c417c9b9c8a83735e07c
Sam Moss <sam.moss321@gmail.com>
2015-11-16 14:04:45 -0600

    functional server

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 /**
  * Created by samuelmoss on 11/16/15.
  */
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', process.env.PORT || 5000);
+
+app.get('/*', function(req,res){
+    var file = req.params[0] || 'views/index.html';
+    res.sendFile(path.join(__dirname,"./public/",file));
+});
+
+app.listen(app.get('port'), function(){
+   console.log("London calling on port: ", app.get("port"));
+});
